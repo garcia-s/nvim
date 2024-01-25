be7b54f5b6084dbdb50289d5035bde31274c5d5d
Juan Garcia <garciajj18@gmail.com>
2024-01-25 18:23:11 -0500

    better formatting

--- after/plugin/formatter.lua
+++ after/plugin/formatter.lua
@@ -1,24 +1,4 @@
--- Utilities for creating configurations
-local formatter = require("formatter")
--- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
-formatter.setup({
-    -- Enable or disable logging
-    logging = true,
-    -- Set the log level
-    log_level = vim.log.levels.WARN,
-    -- All formatter configurations are opt-in
-    filetype = {
-        -- Formatter configurations for filetype "lua" go here
-        -- and will be executed in order
-        css = {
-            require('formatter.filetypes.css').prettierd
-        },
-        -- Use the special "*" filetype for defining formatter configurations on
-        -- any filetype
-        ["*"] = {
-            -- "formatter.filetypes.any" defines default configurations for any
-            -- filetype
-            require("formatter.filetypes.any").remove_trailing_whitespace
-        }
-    }
-})
+local prettier = {
+    formatCommand = 'prettierd "${INPUT}"',
+    formatStdin = true,
+}
--- lua/symmetry/remaps.lua
+++ lua/symmetry/remaps.lua
@@ -1,9 +1,5 @@
-vim.keymap.set("n", "<A-F>", function() 
-    if(vim.bo.filetype == "css" or vim.bo.filetype == "html") then
-        return vim.cmd({cmd= "Format"})
-    end
-    vim.lsp.buf.format({async = true, timeout= 2000})
-
+vim.keymap.set("n", "<A-F>", function()
+    vim.lsp.buf.format({ async = true, timeout = 2000 })
 end)
 vim.keymap.set("n", "<C-s>", ':<c-u>write<cr>')
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
